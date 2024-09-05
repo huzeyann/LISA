7211e998889d4487c41aa408ab3ca2ef01a0c357
huze <huze.yann@gmail.com>
2024-09-05 22:12:09 +0000

    no req

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-# read the requirements file
-import os
-requirement_file = 'requirements.txt'
-with open(requirement_file) as f:
-    required = f.read().splitlines()
+# # read the requirements file
+# import os
+# requirement_file = 'requirements.txt'
+# with open(requirement_file) as f:
+#     required = f.read().splitlines()
     
 setup(
     name="lisa",
     version="0.0.1",
     # packages=['lisa'],
     packages=find_packages(),
-    install_requires=required,
+    # install_requires=required,
 )
